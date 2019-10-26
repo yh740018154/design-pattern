25947ef51e4970134531ac766c6a3c7acad0ee3e
yangheng <yangheng@dragonsoft.com.cn>
2019-10-26 15:02:27 +0800

    设计模式建造者模式学习

--- builder-pattern/src/main/java/cn/com/datu/yangheng/builder/TestBuilder.java
+++ builder-pattern/src/main/java/cn/com/datu/yangheng/builder/TestBuilder.java
@@ -11,6 +11,11 @@ import org.junit.Test;
  */
 public class TestBuilder {
 
+    /**
+     * builder 为一个接口，我们可以传入不同的实现类，然后可以造很多种手机
+     * @param builder
+     * @return
+     */
     @Test
     public MoiblePhone buildMoiblePhone(Builder builder) {
 
